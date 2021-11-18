6b903e77e6a89bab4ae76cd972a5dc71df0f95f3
Bletraut <polarisf@yandex.ru>
2021-11-18 20:49:47 +0300

    Added json and adressables

--- Assets/Scripts/Goods.cs
+++ Assets/Scripts/Goods.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+
+using UnityEngine;
 using UnityEngine.UI;
 
 using TMPro;
@@ -41,4 +43,48 @@ public class Goods : MonoBehaviour
         userLevel.text = $"{goodsData.UserLevel}";
         //UserImage.sprite = ???;
     }
+}
+
+public struct GoodsData
+{
+    public string GoodsName;
+    public int GoodsCount;
+    public int GoodsPrice;
+    public string GoodsImage;
+
+    public string UserName;
+    public int UserLevel;
+    public string UserImage;
+
+    public GoodsData(string goodsName, int goodsCount, int goodsPrice, string goodsImage, string userName, int userLevel, string userImage)
+    {
+        GoodsName = goodsName;
+        GoodsCount = goodsCount;
+        GoodsPrice = goodsPrice;
+        GoodsImage = goodsImage;
+
+        UserName = userName;
+        UserLevel = userLevel;
+        UserImage = userImage;
+    }
+
+    public override string ToString()
+    {
+        return $"{{ GoodsName={GoodsName}, GoodsName={GoodsName},"
+                + $" GoodsPrice={GoodsPrice} GoodsImage={GoodsImage},"
+                + $" UserName={UserName}, UserLevel = {UserLevel}, UserImage={UserImage} }}";
+    }
+
+    private const string symbols = "ABCDEFJHIJKLMNOPQRSTUVWXYZ0123456789";
+    public static GoodsData Random => new GoodsData()
+    {
+        GoodsName = new string(symbols.OrderByDescending(n => UnityEngine.Random.value).Take(UnityEngine.Random.Range(1, 10)).ToArray()),
+        GoodsCount = UnityEngine.Random.Range(1, 100),
+        GoodsPrice = UnityEngine.Random.Range(1, 1000),
+        GoodsImage = new string(symbols.OrderByDescending(n => UnityEngine.Random.value).ToArray()),
+
+        UserName = new string(symbols.OrderByDescending(n => UnityEngine.Random.value).Take(UnityEngine.Random.Range(1, 10)).ToArray()),
+        UserLevel = UnityEngine.Random.Range(1, 100),
+        UserImage = new string(symbols.OrderByDescending(n => UnityEngine.Random.value).ToArray())
+    };
 }
--- Assets/Scripts/NewsWindow.cs
+++ Assets/Scripts/NewsWindow.cs
@@ -1,6 +1,4 @@
-﻿using System.Linq;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
 using UnityEngine;
@@ -41,48 +39,4 @@ public class NewsWindow : MonoBehaviour
             goodsData.Add(goods);
         }
     }
-}
-
-public struct GoodsData
-{
-    public string GoodsName;
-    public int GoodsCount;
-    public int GoodsPrice;
-    public string GoodsImage;
-
-    public string UserName;
-    public int UserLevel;
-    public string UserImage;
-
-    public GoodsData(string goodsName, int goodsCount, int goodsPrice, string goodsImage, string userName, int userLevel, string userImage)
-    {
-        GoodsName = goodsName;
-        GoodsCount = goodsCount;
-        GoodsPrice = goodsPrice;
-        GoodsImage = goodsImage;
-
-        UserName = userName;
-        UserLevel = userLevel;
-        UserImage = userImage;
-    }
-
-    public override string ToString()
-    {
-        return $"{{ GoodsName={GoodsName}, GoodsName={GoodsName},"
-                + $" GoodsPrice={GoodsPrice} GoodsImage={GoodsImage},"
-                + $" UserName={UserName}, UserLevel = {UserLevel}, UserImage={UserImage} }}";
-    }
-
-    private const string symbols = "ABCDEFJHIJKLMNOPQRSTUVWXYZ0123456789";
-    public static GoodsData Random => new GoodsData()
-    {
-        GoodsName = new string(symbols.OrderByDescending(n => UnityEngine.Random.value).Take(UnityEngine.Random.Range(1, 10)).ToArray()),
-        GoodsCount = UnityEngine.Random.Range(1, 100),
-        GoodsPrice = UnityEngine.Random.Range(1, 1000),
-        GoodsImage = new string(symbols.OrderByDescending(n => UnityEngine.Random.value).ToArray()),
-
-        UserName = new string(symbols.OrderByDescending(n => UnityEngine.Random.value).Take(UnityEngine.Random.Range(1, 10)).ToArray()),
-        UserLevel = UnityEngine.Random.Range(1, 100),
-        UserImage = new string(symbols.OrderByDescending(n => UnityEngine.Random.value).ToArray())
-    };
 }

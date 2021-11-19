37495beaa45744ee46708c2c4e2aaa6b2ec166a0
Bletraut <polarisf@yandex.ru>
2021-11-19 21:49:01 +0300

    Commit added json data

--- Assets/Scripts/Goods.cs
+++ Assets/Scripts/Goods.cs
@@ -3,6 +3,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 using TMPro;
 
@@ -33,20 +34,21 @@ public class Goods : MonoBehaviour
         transform.localScale = Vector3.one;
     }
 
-    public async void SetData(GoodsData goodsData)
+    public void SetData(GoodsData goodsData)
     {
         goodsName.text = goodsData.GoodsName;
         goodsCount.text = $"x{goodsData.GoodsCount}";
         goodsPrice.text = $"{goodsData.GoodsPrice}";
-        //goodsImage.sprite = ???;
-
-        var handle = Addressables.LoadAssetAsync<Sprite>("Corn");
-        await handle.Task;
-        goodsImage.sprite = handle.Result;
+        Addressables.LoadAssetAsync<Sprite>(goodsData.GoodsImage).Completed += GoodsImage_Loaded;
 
         userName.text = goodsData.UserName;
         userLevel.text = $"{goodsData.UserLevel}";
         //UserImage.sprite = ???;
+    }
+
+    private void GoodsImage_Loaded(AsyncOperationHandle<Sprite> obj)
+    {
+        goodsImage.sprite = obj.Result;
     }
 }
 
